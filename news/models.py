5d11a6286387f9ba48b5a12586ad4fd443160f00
albright.human <sungutialbright@gmail.com>
2022-05-23 22:02:08 +0300

    ft:add tags model and table in db

--- news/models.py
+++ news/models.py
@@ -5,4 +5,16 @@ class Editor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+class Meta:
+        ordering = ['first_name']
+        
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
     
